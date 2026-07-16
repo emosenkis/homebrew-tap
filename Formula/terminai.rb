f9c78e9b20cacd511872d54447730edb74843ad8
Eitan Mosenkis <eitan@mosenkis.net>
2026-07-16 22:27:36 +0300

    Update terminai to 0.1.7

--- Formula/terminai.rb
+++ Formula/terminai.rb
@@ -2,15 +2,9 @@ class Terminai < Formula
   desc "Interactive terminal wrapper with AI assistant"
   homepage "https://github.com/emosenkis/terminai"
   url "https://github.com/emosenkis/terminai.git",
-      tag:      "v0.1.6",
-      revision: "88bb151d00467682950a238f56e6bd4ca9a87860"
+      tag:      "v0.1.7",
+      revision: "fd10e53535e5cee527b3f82513ea6dcd566c77fd"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/emosenkis/homebrew-tap/releases/download/terminai-0.1.6"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "840ff1f9d8989f1b8ee8eb49bb7f4b47663c4b464f2b07e1bc4fd058ffb3cf33"
-    sha256 cellar: :any,                 x86_64_linux: "59d21ef9c501aada240fa04b9f902bc059f356d67b5f2256cf5ef0da799e92b1"
-  end
 
   depends_on "rust" => :build
 
