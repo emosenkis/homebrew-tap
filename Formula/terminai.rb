447185d01e273df69768d4eec025c0423a51f3c8
Eitan Mosenkis <eitan@mosenkis.net>
2026-07-27 01:57:23 +0300

    terminai 0.1.14
    Closes #10.
    
    Signed-off-by: Eitan Mosenkis <1794126+emosenkis@users.noreply.github.com>

--- Formula/terminai.rb
+++ Formula/terminai.rb
@@ -2,8 +2,8 @@ class Terminai < Formula
   desc "Interactive terminal wrapper with AI assistant"
   homepage "https://github.com/emosenkis/terminai"
   url "https://github.com/emosenkis/terminai.git",
-      tag:      "v0.1.13",
-      revision: "7f96ff46e4a19a1a43e1d9ed7ffbdae2f8b5e5dc"
+      tag:      "v0.1.14",
+      revision: "051ee14cbab90ce3d3d59466e51a3decdecdc08c"
   license "MIT"
 
   bottle do
