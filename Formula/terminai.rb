8abcbb4d60068460d69761c64d11db524e21f9e3
Eitan Mosenkis <1794126+emosenkis@users.noreply.github.com>
2026-07-28 17:04:35 +0000

    terminai: update 0.1.17 bottle.

--- Formula/terminai.rb
+++ Formula/terminai.rb
@@ -7,9 +7,9 @@ class Terminai < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/emosenkis/homebrew-tap/releases/download/terminai-0.1.16"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "651b173b63a9af8f55b659e3573d562340774fe82b80ea78cda3bec9ab1e3715"
-    sha256 cellar: :any,                 x86_64_linux: "849f51956026a42541be528a71d0c7df3cf14f0ac32baa65f0ab6edc190bf18c"
+    root_url "https://github.com/emosenkis/homebrew-tap/releases/download/terminai-0.1.17"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "841a02945a760561848226589b3fac0c967e16a6ae2584e342ad53514574803f"
+    sha256 cellar: :any,                 x86_64_linux: "2f46ff16c7b725a097c74a5d65c47df71d9ea3f258fa344fd8db270670d153fc"
   end
 
   depends_on "rust" => :build
